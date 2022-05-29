e6e4fcc80dfa4cb96df940b87f65afe756038b37
Johannes Lindén <johannes@godesity.se>
2022-05-29 10:41:39 +0200

    import os

--- chromebrowser.py
+++ chromebrowser.py
@@ -1,6 +1,6 @@
 import time
+import os
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
