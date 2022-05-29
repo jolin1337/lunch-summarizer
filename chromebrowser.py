eb027fa85f67e1fb6df22f6266569a469f28bf9d
Johannes Lindén <johannes@godesity.se>
2022-05-29 10:38:16 +0200

    Add chrome arguments

--- chromebrowser.py
+++ chromebrowser.py
@@ -7,9 +7,12 @@ from selenium.webdriver.common.by import By
 
 def load_web_driver() -> webdriver.Chrome:
     # instantiate a chrome options object so you can set the size and headless preference
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     # go to google
     driver = webdriver.Chrome(chrome_options=chrome_options)
